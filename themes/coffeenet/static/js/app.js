f65d84711f4cbf048e614693f44c0d23a88ee57b
Tobias Schneider <tobias.schneider@tobsch.org>
2018-08-09 18:33:27 +0200

    Add service worker scope to '/'

--- themes/coffeenet/static/js/app.js
+++ themes/coffeenet/static/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/js/service-worker.js')
+        .register('/js/service-worker.js', { scope: '/' })
         .then(function () {
             console.log('Service Worker Registered');
         });
